8a51ff3db0bebc8c6172030370dec6b6a07f86b1
Numer <numerpriv@gmail.com>
2022-01-19 16:58:12 +0100

    Added Title and ViewWrapper component

--- src/components/organisms/Form/Form.js
+++ src/components/organisms/Form/Form.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Wrapper, StyledTitle } from '../UsersList/UserList.styles';
+import { Title } from 'components/atoms/Title/Title';
 import FormField from 'components/molecules/FormField/FormField';
 import { Button } from 'components/atoms/Button/Button';
+import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 
 const Form = ({ handleAddUser, formValues, handleInputChange }) => {
   return (
     <>
-      <Wrapper as="form" onSubmit={handleAddUser}>
-        <StyledTitle>Test</StyledTitle>
+      <ViewWrapper as="form" onSubmit={handleAddUser}>
+        <Title>Test</Title>
         <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange}></FormField>
         <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputChange}></FormField>
         <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange}></FormField>
         <Button type="submit">Add</Button>
-      </Wrapper>
+      </ViewWrapper>
     </>
   );
 };
