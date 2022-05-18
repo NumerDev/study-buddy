06125f934443f01507038fd67a35eba2a5a628eb
Mateusz Grzomba <numerpriv@gmail.com>
2022-05-18 22:45:21 +0200

    Created useStudents hook and moved related functions to it

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -4,8 +4,13 @@ import { StyledList } from './UserList.styles';
 import PropTypes from 'prop-types';
 import { Title } from 'components/atoms/Title/Title';
 import { UserShape } from 'types';
+import { useStudents } from 'hooks/useStudents';
+import { useParams } from 'react-router-dom';
 
-const UsersList = ({ users = [] }) => {
+const UsersList = () => {
+  const { id } = useParams();
+  const { students: users } = useStudents({ groupId: id });
+
   return (
     <>
       <Title>Students list</Title>
