6142e3905e49a0a01dcd18025be3eb2990600261
Mateusz Grzomba <numerpriv@gmail.com>
2022-05-18 22:46:04 +0200

    Added search route to MSW and basic functionality of search bar

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import { StatusInfo, SearchBarWrapper } from './SearchBar.styles';
+import React, { useEffect, useState } from 'react';
+import { StatusInfo, SearchBarWrapper, SearchWrapper, SearchResults } from './SearchBar.styles';
 import { Input } from 'components/atoms/Input/Input';
+import { useStudents } from 'hooks/useStudents';
+import debounce from 'lodash.debounce';
 
 export const SearchBar = () => {
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const [matchingStudents, setMatchingStudents] = useState('');
+  const { findStudents } = useStudents();
+
+  const getMatchingStudents = debounce(async (e) => {
+    const { students } = await findStudents(searchPhrase);
+    setMatchingStudents(students);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchPhrase) return;
+    getMatchingStudents(searchPhrase);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchPhrase]);
+
   return (
     <SearchBarWrapper>
       <StatusInfo>
         <p>Logged as:</p>
-        <p className="username">User562</p>
+        <p>
+          <strong>User562</strong>
+        </p>
       </StatusInfo>
-      <Input />
+      <SearchWrapper>
+        <Input
+          onChange={(e) => {
+            setSearchPhrase(e.target.value);
+          }}
+        />
+        {searchPhrase && matchingStudents.length ? (
+          <SearchResults>
+            {matchingStudents.map((student) => (
+              <li key={student.id}>{student.name}</li>
+            ))}
+          </SearchResults>
+        ) : null}
+      </SearchWrapper>
     </SearchBarWrapper>
   );
 };
